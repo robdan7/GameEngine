c787ecb928e1faee97dcb4ed1d152dd9359e6386
Robin <robdan-7@student.ltu.se>
2019-03-16 13:25:43 +0100

    New repository name (probably)

---
+++ Voxagon/src/module-info.java
@@ -0,0 +1,31 @@
+/**
+ * 
+ */
+/**
+ * @author Robin
+ *
+ */
+module Voxagon {
+	exports core.graphics.renderUtils;
+	exports core.graphics.renderUtils.uniforms;
+	exports core.utils.math;
+	exports core.graphics.lights;
+	exports core.utils.other;
+	exports core.utils.datatypes;
+	exports core.graphics.renderUtils.uniforms.old;
+	exports core.utils.event;
+	exports core.input;
+	exports core.graphics.misc;
+	exports core.graphics.renderUtils.buffers;
+	exports core.graphics.ui;
+	exports core.graphics.ui.old;
+	exports core.utils.fileSystem;
+	exports core.engine;
+	exports core.graphics.models;
+
+	requires PNGDecoder;
+	requires java.desktop;
+	requires org.lwjgl;
+	requires org.lwjgl.glfw;
+	requires org.lwjgl.opengl;
+}
