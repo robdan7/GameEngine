d4e9ae8a2ecef734a743077fc0a1a327cae2716b
Robin <robdan-7@student.ltu.se>
2019-03-17 18:51:29 +0100

    Added generic variable in update method

--- Voxagon/src/core/utils/event/Listener.java
+++ Voxagon/src/core/utils/event/Listener.java
@@ -1,5 +1,5 @@
 package core.utils.event;
 
 public interface Listener<T, L extends Listener<T,L,O>,O extends Observer<T,O,L>> {
-	public void update(Observer<T,O,L> b, Object arg);
+	public void update(Observer<T,O,L> b, T arg);
 }
