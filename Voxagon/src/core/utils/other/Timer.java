a4dbc381df81087f7f2a9106e88cedc401534849
Robin <robdan-7@student.ltu.se>
2019-03-20 13:20:37 +0100

    More javadoc and small improvements.

--- Voxagon/src/core/utils/other/Timer.java
+++ Voxagon/src/core/utils/other/Timer.java
@@ -1,9 +1,11 @@
 package core.utils.other;
-//import static org.lwjgl.glfw.GLFW.glfwGetTime;
-import static org.lwjgl.glfw.GLFW.*;
+
+import static org.lwjgl.glfw.GLFW.glfwGetTimerValue;
+import static org.lwjgl.glfw.GLFW.glfwGetTimerFrequency;
 
 /**
- * Timer, or stop watch, that counts time in seconds. Nothing fancy here.
+ * Timer, or stop watch, that counts time in seconds. No wrap-around protection is implemented, 
+ * but this timer should, in theory, be able to run for a year without problems.
  * @author Robin
  *
  */
@@ -11,6 +13,9 @@ public class Timer {
 	private double startTime, currentTime, targetTime, frequency, lastDelta;
 	private boolean running = false;
 	
+	/**
+	 * Create a new timer. It must be started before it can be used.
+	 */
 	public Timer() {
 		
 		// Store a local frequency.
@@ -20,11 +25,12 @@ public class Timer {
 	
 	/**
 	 * Create a timer with a target time. The timer will stop when the target it reached.
+	 * Note: It must be started before it can be used.
 	 * @param target - Target time in seconds.
 	 */
 	public Timer(double target) {
+		this();
 		this.targetTime = target;
-		
 	}
 	
 	public void setTargetTime(double time) {
@@ -56,12 +62,12 @@ public class Timer {
 	}
 	
 	/**
-	 * Return the time in seconds since the timer was started.
+	 * Return the total time in seconds since the timer was started.
 	 * The time stops if a target time has been set.
 	 * @return
 	 */
 	public double getTime() {
-		if (this.running) {
+		if (this.isRunning()) {
 			this.currentTime = glfwGetTimerValue();
 		} 
 		double seconds = (this.currentTime-this.startTime) / this.frequency;
@@ -72,13 +78,21 @@ public class Timer {
 	
 	/**
 	 * Get the delta time since the last call. Measured in seconds.
-	 * @return
+	 * @return - A time value if it is running, 0 otherwise.
 	 */
 	public double getDelta() {
-		double time = glfwGetTimerValue();
-		double delta = time - this.lastDelta;
-		this.lastDelta = time;
-		return delta / this.frequency;
+		if (this.isRunning()) {
+			double time = glfwGetTimerValue();
+			double delta = time - this.lastDelta;
+			this.lastDelta = time;
+			return delta / this.frequency;
+		} else {
+			return 0;
+		}
+	}
+	
+	public boolean isRunning() {
+		return this.running;
 	}
 	
 	/**
