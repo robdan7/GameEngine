23e94b82ace448d8ee105c607f192bcb9d3f90d5
Robin <robdan-7@student.ltu.se>
2019-04-01 17:58:51 +0200

    Added a function for existing models.

--- Voxagon/src/core/graphics/models/ModelCompiler.java
+++ Voxagon/src/core/graphics/models/ModelCompiler.java
@@ -61,6 +61,53 @@ public class ModelCompiler {
 		return model;
 	}
 	
+	public static void loadModelBlueprint(ModelBlueprint model, String f) throws IOException {
+		if (!f.contains(".ini")) {
+			throw new RuntimeException("Unsupported file excension.");
+		}
+		BufferedReader reader = FileManager.getReader(f);
+		String line = "";
+		String[] splittedLine;
+		//ModelBlueprint model;
+		String modelFile = "", textureFile = "", shader = "";
+		while  ((line = reader.readLine()) != null) {
+			
+			line = line.replaceAll("\\s","");
+			if(line.startsWith("//")) {
+				continue;
+			}
+			splittedLine = line.split("=");
+			if (splittedLine.length != 2) {
+				throw new IOException(splittedLine[0] + " property is empty in " + f);
+			}
+			
+			/*
+			 * See if the line contains a file path. Put that file path in a string which then can be
+			 * used to create the model with associated shaders and textures.
+			 */
+			if (splittedLine[0].contains(Settings.MODEL.getName())) {
+				modelFile = splittedLine[1];
+			} else if (splittedLine[0].contains(Settings.TEXTURE.getName())) {
+				textureFile = splittedLine[1];
+			} else if (splittedLine[0].contains(Settings.SHADER.getName())) {
+				shader = splittedLine[1];
+			}
+		}
+		reader.close();
+		
+		// Create a private shader program for the model.
+		Shaders shaderProgram = new Shaders(shader);
+		
+		reader.close();
+		
+		// Create the model and return it.
+		//model = new ModelBlueprint(modelFile, textureFile);
+		
+		model.setModel(modelFile);
+		model.setTexture(textureFile);
+		model.setShader(shaderProgram);
+	}
+	
 	/**
 	 * This represents the available settings in a model ini file.
 	 * @author Robin
