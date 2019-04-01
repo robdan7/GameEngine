315cb490e0a8562d18d58864e3467883270a3a95
Robin <robdan-7@student.ltu.se>
2019-04-01 17:49:02 +0200

    Added functions for collision calculation.

--- Voxagon/src/core/physics/collision/CollisionMesh.java
+++ Voxagon/src/core/physics/collision/CollisionMesh.java
@@ -1,9 +1,49 @@
 package core.physics.collision;
 
-public class CollisionMesh {
+import core.graphics.models.Model;
+import core.graphics.models.ModelBlueprint;
+import core.utils.math.Vector3f;
 
-	public CollisionMesh() {
-		// TODO Auto-generated constructor stub
+public class CollisionMesh extends CollisionObject{
+	private ModelBlueprint m;
+	private Vector3f postition;
+
+	public CollisionMesh(ModelBlueprint m) {
+		this.m = m;
+	}
+
+	@Override
+	public Vector3f getFurthestPoint(Vector3f direction) {
+		Vector3f result = m.getVertices().get(0).copy();
+		result.add(this.getPosition());
+		float dot = result.dot(direction);
+		float newDot = dot;
+		
+		
+		for (Vector3f v : m.getVertices()) {
+			if ((newDot = v.asAdded(this.getPosition()).dot(direction)) >= dot) {
+				dot = newDot;
+				result.set(v);
+				result.add(this.getPosition());
+			}
+		}
+		
+		return result;
+	}
+
+	@Override
+	public float getfurthestDistance(Vector3f direction) {
+		return 0;
+	}
+
+	@Override
+	public Vector3f getPosition() {
+		return this.m.getPosition();
+	}
+
+	@Override
+	public void setPosition(Vector3f position) {
+		this.postition.set(position);
 	}
 
 }
--- Voxagon/src/core/physics/collision/CollisionObject.java
+++ Voxagon/src/core/physics/collision/CollisionObject.java
@@ -1,5 +1,7 @@
 package core.physics.collision;
 
+import core.utils.math.Vector3f;
+
 public abstract class CollisionObject {
 	private BoundingSphere fastCollisionSphere;
 
@@ -10,5 +12,20 @@ public abstract class CollisionObject {
 	BoundingSphere getCollisionSphere() {
 		return this.fastCollisionSphere;
 	}
+	
+	/**
+	 * Retrieve the furthest point in a certain direction. The 
+	 * position of the point on a plane perpendicular to the direction can 
+	 * be anywhere, as long as no other point is on the other side of the plane normal 
+	 * (the negative direction).
+	 * @param direction
+	 * @return
+	 */
+	public abstract Vector3f getFurthestPoint(Vector3f direction);
+	
+	public abstract float getfurthestDistance(Vector3f direction);
+	
+	public abstract Vector3f getPosition();
 
+	public abstract void setPosition(Vector3f position);
 }
