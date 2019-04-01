e4423ccd594296a2c3b5fc96cac33b7aa411003c
Robin <robdan-7@student.ltu.se>
2019-04-01 17:48:30 +0200

    Added functions for collision calculation

--- Voxagon/src/core/physics/collision/BoundingSphere.java
+++ Voxagon/src/core/physics/collision/BoundingSphere.java
@@ -1,13 +1,43 @@
 package core.physics.collision;
 
-public class BoundingSphere extends CollisionObject{
+import core.utils.math.Vector3f;
+import core.utils.math.geometry.Sphere;
 
-	public BoundingSphere() {
-		// TODO Auto-generated constructor stub
+public class BoundingSphere extends CollisionObject{
+	private Sphere collisionSphere;
+	
+	public BoundingSphere(Vector3f offset, float radius) {
+		this.collisionSphere = new Sphere(offset, radius);
 	}
 	
 	BoundingSphere getCollisionSphere() {
 		return this;
+	}
+	
+	public float getRadius() {
+		return this.collisionSphere.getRadius();
+	}
+
+	@Override
+	public Vector3f getFurthestPoint(Vector3f direction) {
+		Vector3f result = direction.asMultiplied(this.collisionSphere.getRadius());
+		result.add(this.collisionSphere.getPosition());
+		return result;
+	}
+
+	@Override
+	public float getfurthestDistance(Vector3f direction) {
+		return this.collisionSphere.getRadius()+this.collisionSphere.getPosition().dot(direction);
+	}
+
+	@Override
+	public Vector3f getPosition() {
+		return this.collisionSphere.getPosition();
+	}
+
+	@Override
+	public void setPosition(Vector3f position) {
+		this.getCollisionSphere().setPosition(position);
 	}
 
 }
