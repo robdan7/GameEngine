c3d52a91d83ab23cdc2075cf03323016f0afacdd
Robin <robdan-7@student.ltu.se>
2019-04-02 19:54:01 +0200

    Created XML reader.

---
+++ Voxagon/src/core/utils/fileSystem/XMLparser.java
@@ -0,0 +1,33 @@
+package core.utils.fileSystem;
+
+import java.io.*;
+import org.w3c.dom.*;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.*;
+
+public class XMLparser {
+	
+	private static DocumentBuilder getBuilder() throws ParserConfigurationException {
+		DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+		return factory.newDocumentBuilder();
+	}
+	
+	/**
+	 * Create a XML document from the input file.
+	 * @param file - The file to read from.
+	 * @return A XML document readable in Java.
+	 * @throws SAXException
+	 * @throws IOException
+	 * @throws ParserConfigurationException
+	 */
+	public static Document createParser(String file) throws SAXException, IOException, ParserConfigurationException {
+		InputStream stream = FileManager.getStream(file);
+
+		DocumentBuilder builder = getBuilder();
+		Document doc = builder.parse(stream);
+		stream.close();
+		
+		return doc;
+	}
+}
