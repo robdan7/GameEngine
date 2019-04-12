bf06573a5148e32d3578beeb449175b12fe799c1
Robin <robdan-7@student.ltu.se>
2019-04-12 19:43:48 +0200

    Removed an exception and put it in a separate class.

---
+++ Voxagon/src/core/graphics/shading/uniforms/UniformCreationException.java
@@ -0,0 +1,13 @@
+package core.graphics.shading.uniforms;
+
+public class UniformCreationException extends Exception {
+	private static final long serialVersionUID = 1L;
+	
+	UniformCreationException() {
+		super();
+	}
+	
+	UniformCreationException(String s) {
+		super(s);
+	}
+}
